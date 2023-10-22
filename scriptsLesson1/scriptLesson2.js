c2b1204b44efcc38f66505d5a9a318d073e60319
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-10-22 10:44:13 +0300

    Result of Lesson 10 with Homework

--- scriptsLesson1/scriptLesson2.js
+++ scriptsLesson1/scriptLesson2.js
@@ -1,18 +1,21 @@
-const myPromise = new Promise((res, rej) => {
-    setTimeout(() => {
-            res()
-        }
-        , 2000
-    )
-})
-
-myPromise
-    .then(() => {
-        console.log("cucu1")
-        return "cucu"
-    })
-    .then((data) => {
-        console.log("cucu2", data)
-    })
+// const myPromise = new Promise((res, rej) => {
+//     setTimeout(() => {
+//             res()
+//         }
+//         , 1000
+//     )
+// })
+//
+// myPromise
+//     .then(=> {
+//         console.log("lava")
+//         res()
+//     })
 
 
+Promise.reject("reject1")
+    .catch((t) =>t+"catch1") //rejec1catch1
+    .catch((t) =>t+"catch2")
+    .then((t) =>t+"than1") //rejec1catch1than1
+    .finally((t) =>t+"finally")
+    .then((t) =>console.log(t)) // rejec1catch1than1
