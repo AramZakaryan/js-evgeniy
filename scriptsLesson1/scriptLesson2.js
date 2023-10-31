c5985bb07a93bba47fb9ae0b1c3f48b8cf8a6d89
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-10-31 22:57:25 +0200

    Repetition of Lesson 10

--- scriptsLesson1/scriptLesson2.js
+++ scriptsLesson1/scriptLesson2.js
@@ -13,9 +13,39 @@
 //     })
 
 
-Promise.reject("reject1")
-    .catch((t) =>t+"catch1") //rejec1catch1
-    .catch((t) =>t+"catch2")
-    .then((t) =>t+"than1") //rejec1catch1than1
-    .finally((t) =>t+"finally")
-    .then((t) =>console.log(t)) // rejec1catch1than1
+// Promise.reject("reject1")
+//     .catch((t) =>t+"catch1") //rejec1catch1
+//     .catch((t) =>t+"catch2")
+//     .then((t) =>t+"than1") //rejec1catch1than1
+//     .finally((t) =>t+"finally")
+//     .then((t) =>console.log(t)) // rejec1catch1than1
+
+
+const myPromise = new Promise(
+    (res, rej) => {
+        setTimeout(() => {
+                let result = 1
+                console.log(result)
+                res(result)
+            }
+            , 2000
+        )
+
+    }
+)
+    .then((arg2) => {
+            new Promise(
+                (res, rej) => {
+                    setTimeout(() => {
+                            let result2 = ++arg2
+                            console.log(result2)
+                            res(result2)
+                        }
+                        , 2000
+                    )
+
+                }
+            )
+
+        }
+    )
