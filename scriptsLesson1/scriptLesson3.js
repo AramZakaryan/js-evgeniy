ee6c2ef78a1737af96a8c97d0dbec60490d8bc94
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-11-28 20:08:53 +0400

    Interim Result of Lesson 11

--- scriptsLesson1/scriptLesson3.js
+++ scriptsLesson1/scriptLesson3.js
@@ -1,10 +1,18 @@
+//////// equivalent of all static method of promise
+
 const promise1 = new Promise((res, rej) => res("res1"))
 const promise2 = new Promise((res, rej) => res("res2"))
 const promise3 = new Promise((res, rej) => res("res3"))
 
 
-Promise.resolve([])
-    .then(data => promise1.then(d => [...data, d]))
-    .then(data => promise2.then(d => [...data, d]))
-    .then(data => promise3.then(d => [...data, d]))
-    .then(data => console.log(data))
+const p0 = Promise.resolve([])
+
+
+const p1 = p0.then(data => promise1.then(d => [...data, d]))
+const p2 = p1.then(data => promise2.then(d => [...data, d]))
+const p3 = p2.then(data => promise3.then(d => [...data, d]))
+
+p3.then(data => console.log(data))
+    .catch(err => console.log("eh", err))
+
+
