4fdf4bd5cd9e970cdbc4199b4157d7d8e1172bb2
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-11-28 19:54:43 +0400

    Interim Result of Lesson 11

--- scriptsLesson1/scriptLesson3.js
+++ scriptsLesson1/scriptLesson3.js
@@ -1,61 +1,10 @@
-// const myPromise1 = new Promise((res, rej) => {
-//     setTimeout(() => {
-//             let result = 100
-//              res( result)
-//         }
-//         , ~~(1000 + Math.random() * 2000))
-// })
-//
-// const myPromise2 = new Promise((res, rej) => {
-//     setTimeout(() => {
-//             let result = 200
-//             res( result)
-//         }
-//         , ~~(1000 + Math.random() * 2000))
-// })
-//
-// const myPromise3 = new Promise((res, rej) => {
-//     setTimeout(() => {
-//             let result = 300
-//             res( result)
-//         }
-//         , ~~(1000 + Math.random() * 2000))
-// })
-//
-//
-// Promise.any([myPromise1,myPromise2,myPromise3]).then((data)=>{
-//     console.log(data)})
+const promise1 = new Promise((res, rej) => res("res1"))
+const promise2 = new Promise((res, rej) => res("res2"))
+const promise3 = new Promise((res, rej) => res("res3"))
 
 
-
-// function bigFunc  () {
-//
-//     let count = 0
-//
-//     function smallFunc () {
-//
-//         count++
-//         console.log(count)
-//     }
-//
-//     return smallFunc
-//
-// }
-//
-//
-// const outFunc = bigFunc()
-//
-// outFunc()
-// outFunc()
-// outFunc()
-
-
-console.log("start")
-
-setTimeout(function(){
-    console.log("inside Timeout")
-}
-,0)
-
-console.log("finish")
-
+Promise.resolve([])
+    .then(data => promise1.then(d => [...data, d]))
+    .then(data => promise2.then(d => [...data, d]))
+    .then(data => promise3.then(d => [...data, d]))
+    .then(data => console.log(data))
