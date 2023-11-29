14be6a07728b0f1c5a66642f7f44794fa65dbac1
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-11-29 19:49:45 +0400

    Interim Result of Lesson 11

--- scriptsLesson1/scriptLesson3.js
+++ scriptsLesson1/scriptLesson3.js
@@ -1,18 +1,27 @@
-//////// equivalent of all static method of promise
+// //////// equivalent of all static method of promise
+//
+// const promise1 = new Promise((res, rej) => res("res1"))
+// const promise2 = new Promise((res, rej) => res("res2"))
+// const promise3 = new Promise((res, rej) => res("res3"))
+//
+//
+// const p0 = Promise.resolve([])
+//
+//
+// const p1 = p0.then(data => promise1.then(d => [...data, d]))
+// const p2 = p1.then(data => promise2.then(d => [...data, d]))
+// const p3 = p2.then(data => promise3.then(d => [...data, d]))
+//
+// p3.then(data => console.log(data))
+//     .catch(err => console.log("eh", err))
 
-const promise1 = new Promise((res, rej) => res("res1"))
-const promise2 = new Promise((res, rej) => res("res2"))
-const promise3 = new Promise((res, rej) => res("res3"))
 
 
-const p0 = Promise.resolve([])
 
 
-const p1 = p0.then(data => promise1.then(d => [...data, d]))
-const p2 = p1.then(data => promise2.then(d => [...data, d]))
-const p3 = p2.then(data => promise3.then(d => [...data, d]))
-
-p3.then(data => console.log(data))
-    .catch(err => console.log("eh", err))
-
+(async () => {
+    const pr1 = Promise.resolve("rrr")
+    return await pr1
+})()
+    .then(data => console.log(data))
 
