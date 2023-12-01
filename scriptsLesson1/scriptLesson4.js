ac9f2494291e12095bdf4872d7a736aa22097927
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-12-01 23:14:14 +0400

    Final Result of Lesson 12

--- scriptsLesson1/scriptLesson4.js
+++ scriptsLesson1/scriptLesson4.js
@@ -1,67 +1,106 @@
-// setTimeout(() => {
-//         console.log('s1'); // 1
-//     },
-//     0)
+// // setTimeout(() => {
+// //         console.log('s1'); // 1
+// //     },
+// //     0)
+// //
+// //
+// // setTimeout(() => {
+// //     console.log('s2') // 2
+// // }, 1000)
+// //
+// // new Promise(function (res, rej) {
+// //     console.log('p1'); // 3
+// //     res();
+// //     console.log('p2'); // 4
+// // })
+// //     .then(() => console.log("p3")) // 5
+// //
+// //
+// // console.log("w1") // 6
+// //
+// // async function test1() {
+// //     console.log("a1"); // 7
+// //     await test2();
+// //     console.log("a2"); // 8
+// // }
+// //
+// // async function test2() {
+// //     console.log("a3"); // 9
+// // }
+// //
+// // test1();
+// //
+// // console.log("w2"); // 10
+// //
+// //
+// // // p1 p2 w1   a1 a3 w3     micro p3 a2            macro s1(0) s2(1000)
+// //
+// // // 3 4 6 7 9 10   5 8   1 2     
+// //
+// //
 //
 //
+// const myPromise = (delay) => {
+//     return new Promise((res, rej) => {
+//             setTimeout(res, delay)
+//         }
+//     )
+// }
+//
 // setTimeout(() => {
-//     console.log('s2') // 2
+//     console.log("in setTimeout 1 (1)")
 // }, 1000)
 //
-// new Promise(function (res, rej) {
-//     console.log('p1'); // 3
-//     res();
-//     console.log('p2'); // 4
-// })
-//     .then(() => console.log("p3")) // 5
+//
+// myPromise(1000).then(() => 
+//     console.log("in Promise 1 (2)"));
+//
+// setTimeout(() => {
+//     console.log("in setTimeout 2 (3)");
+// }, 1000) // version1: 100ms // version2 and version3: 1000ms
 //
 //
-// console.log("w1") // 6
+// myPromise(1000).then(() =>
+//     console.log("in Promise 2 (4)")); // version1 and version: 2000ms // version3: 1000ms
 //
-// async function test1() {
-//     console.log("a1"); // 7
-//     await test2();
-//     console.log("a2"); // 8
-// }
+// // version1: 3(s2)[100ms] (1)s1[1000ms] (2)p1(1000ms) (4)[p2](ms2000)
 //
-// async function test2() {
-//     console.log("a3"); // 9
-// }
+// // version2:   (1)s1[1000] (2)p1[1000ms] (3)s2[1000ms] (4)p2[2000ms]     
 //
-// test1();
-//
-// console.log("w2"); // 10
-//
-//
-// // p1 p2 w1   a1 a3 w3     micro p3 a2            macro s1(0) s2(1000)
-//
-// // 3 4 6 7 9 10   5 8   1 2     
+// // version3: (1)s1[1000] (2)p1[1000ms] (3)s2[1000ms] (4)p2[2000ms]
 //
 //
 
 
-const myPromise = (delay) => {
-    return new Promise((res, rej) => {
-            setTimeout(res, delay)
-        }
-    )
-}
+console.log(1);
 
 setTimeout(() => {
-    console.log("in setTimeout1")
-}, 1000)
+    console.log(2);
+    Promise.resolve().then(() => console.log(3))
+})
 
-
-myPromise(1000).then(() => console.log("in Promise 1"));
+new Promise((res, rej) => {
+    console.log(4);
+    res(5)
+})
+    .then((data) => {
+        console.log(data);
+        Promise.resolve()
+            .then(() => {
+                console.log(6);
+            })
+            .then(() => {
+                console.log(7);
+                setTimeout(() => {
+                    console.log(8);
+                }, 0)
+            })
+    })
 
 setTimeout(() => {
-    console.log("in setTimeout2");
-}, 100)
+    console.log(9);
+})
 
+console.log(10);
 
-myPromise(2000).then(() =>
-    console.log("in Promise 2"));
-
-
-
-//   s2 s1 p1 p2                    micro                 macro s2 (100) s1(1000) p1(1000) p2(2000)
+// 1 4 10 5 6 7 2(Oms) 3(0ms)micro 9(0ms) 8(0ms)
