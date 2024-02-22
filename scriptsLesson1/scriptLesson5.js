69c7ba95c61ac0f7f60564cef32e2dfd1ffac35f
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-02-22 23:07:34 +0400

    Interim Commit of Lesson Sprint-04 Week-02

--- scriptsLesson1/scriptLesson5.js
+++ scriptsLesson1/scriptLesson5.js
@@ -1,6 +1,65 @@
+// // // //
+// // // // console.log(this)
+// // //
+// // //
+// // // const foo = ()=>{console.log(this)}
+// // //
+// // // foo()
+// // //
+// // //
+// //
+// // "use strict"
+// //
+// // const myObj = {
+// //     key1: "value1",
+// //     key2: "value2",
+// //     goo
+// // }
+// //
+// // function goo() {
+// //     console.log(this)
+// // }
+// //
+// // goo()
+//
+//
+// const car = {
+//     brand: "bmw",
+//     showThis: () =>{
+//         console.log(this)
+//     }
+// }
+//
+// car.showThis()
 
-// console.log(this)
 
-const foo = ()=>{console.log(this)}
+const car1 = {
+    brand: "bmw", 
+    showBrand() {
+        console.log(this.brand)
+    }
+}
 
-foo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
